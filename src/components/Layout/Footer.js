9ee92af9a5043f0ea98b52fb4b3a2cc5acb01553
Bhavik002 <20cs002@charusat.edu.in>
2023-08-04 22:25:48 +0530

    asdf

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="cright" className="d-flex align-items-center justify-content-center text-light p-4">
+      <h6>Made by Bhavik </h6>
+    </div>
+  );
+};
+//p4 is thickness of border
+export default Footer;
